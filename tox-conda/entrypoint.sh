51ff6472e8f09e9836e4fd03b3dba0b38e004095
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-05-24 00:32:00 +0800

    Update entrypoint.sh

--- tox-conda/entrypoint.sh
+++ tox-conda/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 test $GAUSSIANURL && git clone --depth 1 https://github.com/circulosmeos/gdown.pl && ./gdown.pl/gdown.pl $GAUSSIANURL "g16.tgz">/dev/null 2>&1 && tar -xzvf g16.tgz && g16root=$(pwd) source g16/bsd/g16.profile || :
 python -m tox
