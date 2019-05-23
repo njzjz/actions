8b1c411ee828ab966c0b1b7e524e092e8603d78c
Jinzhe Zeng <jzzeng@stu.ecnu.edu.cn>
2019-05-23 23:37:22 +0800

    Create entrypoint.sh

---
+++ tox-conda/entrypoint.sh
@@ -0,0 +1,2 @@
+test $GAUSSIANURL && git clone --depth 1 https://github.com/circulosmeos/gdown.pl && ./gdown.pl/gdown.pl $GAUSSIANURL "g16.tgz">/dev/null 2>&1 && tar -xzvf g16.tgz && g16root=$(pwd) source g16/bsd/g16.profile || :
+python -m tox
